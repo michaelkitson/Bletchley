f6c1a709771ed601664b0ec6077076d055c5eabf
Michael Kitson <michaelskitson@gmail.com>
2013-02-05 19:31:04 -0500

    Add sha3sum, used to calculate the has of a file

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ LDFLAGS = -lstdc++
 CXX = g++
 CC = g++
 
-all: TestSHA3 HashSHA3 HashZeroBytes LongTest
+all: TestSHA3 HashSHA3 HashZeroBytes LongTest sha3sum
 
 debug: override CPPFLAGS += -ggdb
 debug: all
@@ -16,10 +16,11 @@ TestSHA3: TestSHA3.o SHA3.o
 HashSHA3: HashSHA3.o SHA3.o
 HashZeroBytes: HashZeroBytes.o SHA3.o
 LongTest: LongTest.o SHA3.o
+sha3sum: sha3sum.o SHA3.o
 
 .PHONY: clean realclean rc debug all o3 SHA3-o3
 clean:
-	rm SHA3.o TestSHA3.o HashSHA3.o HashZeroBytes.o LongTest.o
+	rm SHA3.o TestSHA3.o HashSHA3.o HashZeroBytes.o LongTest.o sha3sum.o
 rc: realclean
 realclean: clean
-	rm TestSHA3 HashSHA3 HashZeroBytes LongTest
+	rm TestSHA3 HashSHA3 HashZeroBytes LongTest sha3sum
---
+++ sha3sum.cpp
@@ -0,0 +1,76 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include "HashFunction.h"
+#include "SHA3.h"
+
+#define DEFAULT_DIGEST_BITS 256
+#define FILE_BUFFER_BYTES 512
+
+void usage(){
+    std::cout << "Usage: sha3sum [-a <digestSize>] <file>" << std::endl 
+              << "digestSize defaults to " << DEFAULT_DIGEST_BITS << " bits" << std::endl;
+}
+
+char *hexDigestForFile( const char *filename, const int digestBytes ){
+    std::ifstream file;
+    char buffer[FILE_BUFFER_BYTES];
+    int amountRead;
+
+    SHA3 sha3( digestBytes );
+    HashFunction *hash = &sha3;
+
+    file.open( filename, std::ifstream::in );
+    if( !file.is_open() ){
+        std::cout << "Couldn't open file: " << filename << std::endl;
+        return 0;
+    }
+    while( file.good() && amountRead > 0 ){
+        amountRead = file.readsome( buffer, FILE_BUFFER_BYTES );
+        for( int i = 0; i < amountRead; i++ ){
+            hash->hash( (int) ((unsigned char) buffer[i]) );
+        }
+    }
+    file.close();
+
+    unsigned char *digest = new unsigned char[digestBytes];
+    char *hexDigest = new char[2*digestBytes + 1];
+    char *hexLookup = (char*)"0123456789abcdef";
+    hexDigest[2*digestBytes] = '\0';
+    hash->digest( digest );
+    for( int byte = 0; byte < digestBytes; byte++ ){
+        hexDigest[2*byte]   = hexLookup[digest[byte] >> 4];
+        hexDigest[(2*byte)+1] = hexLookup[digest[byte] & 15];
+    }
+    delete( digest );
+    return hexDigest;
+}
+
+int main( int argc, char *argv[] ){
+    char *filename;
+    int digestSize;
+
+    if( argc == 2 ){
+        digestSize = DEFAULT_DIGEST_BITS;
+        filename = argv[1];
+    }
+    else if( argc == 4 && strcmp( argv[1], "-a" ) == 0 ){
+        digestSize = atoi( argv[2] );
+        if( digestSize <= 0 ){
+            std::cout << digestSize << " is not a valid SHA3 digest length" << std::endl;
+            return 1;
+        }
+        filename = argv[3];
+    }
+    else{
+        usage();
+        return 0;
+    }
+
+    char *hexDigest = hexDigestForFile( filename, digestSize/8 );
+    if( hexDigest != 0 ){
+        std::cout << hexDigest << "\t" << filename << std::endl;
+    }
+    delete( hexDigest );
+    return 0;
+}
